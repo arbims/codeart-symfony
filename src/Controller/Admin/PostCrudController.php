a47e94865be3200af4bfd161178d61a9722dad10
elarbi <elarbi.msahel@cgi.com>
2024-05-16 12:05:58 +0200

    integration ckeditor

--- src/Controller/Admin/PostCrudController.php
+++ src/Controller/Admin/PostCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Post;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
@@ -12,6 +13,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Vich\UploaderBundle\Form\Type\VichFileType;
 
 class PostCrudController extends AbstractCrudController
@@ -19,6 +21,11 @@ class PostCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return Post::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud->addFormTheme('@FOSCKEditor/Form/ckeditor_widget.html.twig');
     }
 
 
@@ -29,13 +36,12 @@ class PostCrudController extends AbstractCrudController
             TextField::new('slug')->setColumns(6),
             TextField::new('imageFile')->setFormType(VichFileType::class)->onlyOnForms(),
             ImageField::new('image')->setBasePath('uploads/posts')->onlyOnIndex(),
-            TextEditorField::new('content')->setColumns(12),
+            TextEditorField::new('content')->hideOnIndex()->setFormType(CKEditorType::class)->setColumns(12),
             AssociationField::new('user')->onlyOnIndex(),
             AssociationField::new('category')->autocomplete()->setColumns(6),
-            ChoiceField::new('type')->setChoices(['post' => '1', 'tutoriel' => '2'])->setColumns(6)->renderAsBadges()->setFormTypeOption('data','1'),
+            ChoiceField::new('type')->setChoices(['post' => '1', 'tutoriel' => '2'])->setColumns(6)->renderAsBadges()->setFormTypeOption('data', '1'),
             AssociationField::new('technologies')->autocomplete()->setColumns(6),
             BooleanField::new('online')->setColumns(6),
         ];
     }
-
 }
--- src/Controller/Admin/TutorielCrudController.php
+++ src/Controller/Admin/TutorielCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Post;
 use App\Entity\Tutoriel;
 use App\Form\PostType;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
@@ -22,6 +23,11 @@ class TutorielCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return Tutoriel::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud->addFormTheme('@FOSCKEditor/Form/ckeditor_widget.html.twig');
     }
 
     public function configureFields(string $pageName): iterable
